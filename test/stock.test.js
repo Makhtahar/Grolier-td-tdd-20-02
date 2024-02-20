e4dac5ff17a7b3116d024d8fb0395c2dcaf5425c
Makhtahar <74063244+Makhtahar@users.noreply.github.com>
2024-02-20 20:55:10 +0100

    Change of value to make it easier to read

--- test/stock.test.js
+++ test/stock.test.js
@@ -4,27 +4,27 @@ const expect = require("chai").expect;
 describe("Testing the Stock function", function () {
   it("1. add stock", function (done) {
     Stock.init([]);
-    Stock.addArticle({ id: 1, name: "toto", price: 15.4, quantity: 1 })
-      .addArticle({ id: 2, name: "tata", price: 25, quantity: 4 })
-      .addArticle({ id: 1, name: "toto", price: 15.4, quantity: 2 });
+    Stock.addArticle({ id: 1, name: "chocolat", price: 16, quantity: 1 })
+      .addArticle({ id: 2, name: "miel", price: 30, quantity: 4 })
+      .addArticle({ id: 1, name: "chocolat", price: 16, quantity: 2 });
 
     expect(Stock.stock).to.deep.equal([
-      { id: 1, name: "toto", price: 15.4, quantity: 3 },
-      { id: 2, name: "tata", price: 25, quantity: 4 },
+      { id: 1, name: "chocolat", price: 16, quantity: 3 },
+      { id: 2, name: "miel", price: 30, quantity: 4 },
     ]);
     done();
   });
 
   it("2. remove stock", function (done) {
     Stock.init([
-      { id: 1, name: "toto", price: 15.4, quantity: 3 },
-      { id: 2, name: "tata", price: 25, quantity: 4 },
+      { id: 1, name: "chocolat", price: 16, quantity: 3 },
+      { id: 2, name: "miel", price: 30, quantity: 4 },
     ]);
 
     Stock.delArticle(1, 2).delArticle(2, 10).delArticle(2, 1);
     expect(Stock.stock).to.deep.equal([
-      { id: 1, name: "toto", price: 15.4, quantity: 1 },
-      { id: 2, name: "tata", price: 25, quantity: 3 },
+      { id: 1, name: "chocolat", price: 16, quantity: 1 },
+      { id: 2, name: "miel", price: 30, quantity: 3 },
     ]);
 
     done();
